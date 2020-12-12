21b1e43d95161cacf60532dfa9125c09121cccc7
unknown <piper.1.hayden@gmail.com>
2020-12-11 20:24:40 -0600

    adding comments

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -7,6 +7,7 @@ import Axios from "axios";
 import HeroTwo from "../components/General/HeroTwo";
 import Content from "../components/General/Content";
 
+//Component that holds contact form
 class ContactPage extends React.Component {
 
     constructor(props) {
@@ -20,10 +21,10 @@ class ContactPage extends React.Component {
         }
     }
 
-    handleChange = (event) => {
 
+    handleTextChange = (event) => {
         const target = event.target;
-        const value = target.type === 'checkbox' ? target.checked : target.value;
+        const value =  target.value;
         const name = target.name;
 
         this.setState({
@@ -32,7 +33,7 @@ class ContactPage extends React.Component {
 
     }
 
-    handleSubmit = (event) => {
+    handleFormSubmit = (event) => {
         event.preventDefault();
         this.setState({
             disabled: true,
@@ -64,25 +65,28 @@ class ContactPage extends React.Component {
         return(
             <div>
                 <HeroTwo title={this.props.title} />
+                {/* Generic Container */}
                 <Content>
-                    <Form onSubmit={this.handleSubmit}>
+                    {/* Contact Form */}
+                    <Form onSubmit={this.handleFormSubmit}>
                         <Form.Group>
                             <Form.Label htmlFor="full-name">Full Name</Form.Label>
-                            <Form.Control id="full-name" name="name" type="text value" value={this.state.name} onChange={this.handleChange}/>
+                            <Form.Control id="full-name" name="name" type="text value" value={this.state.name} onChange={this.handleTextChange}/>
                         </Form.Group>
 
                         <Form.Group>
                             <Form.Label htmlFor="email">Email Address</Form.Label>
-                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleChange}/>
+                            <Form.Control id="email" name="email" type="email" value={this.state.email} onChange={this.handleTextChange}/>
                         </Form.Group>
 
                         <Form.Group>
                             <Form.Label htmlFor="message">Message</Form.Label>
-                            <Form.Control id="message" name="message" as="textarea" rows="4" value={this.state.message} onChange={this.handleChange}/>
+                            <Form.Control id="message" name="message" as="textarea" rows="4" value={this.state.message} onChange={this.handleTextChange}/>
                         </Form.Group>
 
                         <Button className="d-inline-block" type="submit" disabled={this.state.disabled}>Send</Button>
 
+                        {/* Display messages when the message sends or does not send */}
                         {this.state.emailSent === true && <p className="d-inline msg-success">Email Sent</p>}
                         {this.state.emailSent === false && <p className="d-inline msg-failed">Email Did Not Send</p>}
                     </Form>
